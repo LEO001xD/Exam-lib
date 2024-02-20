2d589b64f168f43ed5611c5ff698d2b79f459731
LEO001xD <141621669+LEO001xD@users.noreply.github.com>
2024-02-20 19:42:12 +0700

    Update leo_exam.h

--- src/leo_exam.h
+++ src/leo_exam.h
@@ -1,12 +1,23 @@
-void dx100_work2_setup();
-void dx100_work2_loop();
-//-----------------------
-void dx100_work3_setup();
-void dx100_work3_loop();
-//-----------------------
-void dx100_work4_setup();
-void dx100_work4_loop();
-//-----------------------
-void dx100_work5_setup();
-void dx100_work5_loop();
+#include "LiquidCrystal.h"
+#include <cstdint>
 
+namespace work2
+{
+void dx100_setup(const uint8_t ldr_work2, const uint8_t led_work2[], const uint8_t arrayLength);
+void dx100_loop(const uint8_t ldr_work2, const uint8_t led_work2[], const uint8_t arrayLength);
+} // namespace work2
+namespace work3
+{
+void dx100_setup(const uint8_t led1_work3, const uint8_t led2_work3, const uint8_t led3_work3);
+void dx100_loop(const uint8_t led1_work3, const uint8_t led2_work3, const uint8_t led3_work3);
+} // namespace work3
+namespace work4
+{
+void dx100_setup(const uint8_t servo_work4);
+void dx100_loop(const uint8_t servo_work4);
+} // namespace work4
+namespace work5
+{
+void dx100_setup(const uint8_t potentio_work5, const LiquidCrystal lcd);
+void dx100_loop(const uint8_t potentio_work5, const LiquidCrystal lcd);
+} // namespace work5
