2113db1992f5ed4d78a266ebc7a32577d680f9fd
LEO001xD <141621669+LEO001xD@users.noreply.github.com>
2024-02-20 15:01:55 +0700

    Update leo_exam.hq

--- src/leo_exam.h
+++ src/leo_exam.h
@@ -1,12 +1,12 @@
-void 001xd_work2_setup();
-void 001xd_work2_loop();
+void dx100_work2_setup();
+void dx100_work2_loop();
 //-----------------------
-void 001xd_work3_setup();
-void 001xd_work3_loop();
+void dx100_work3_setup();
+void dx100_work3_loop();
 //-----------------------
-void 001xd_work4_setup();
-void 001xd_work4_loop();
+void dx100_work4_setup();
+void dx100_work4_loop();
 //-----------------------
-void 001xd_work5_setup();
-void 001xd_work5_loop();
+void dx100_work5_setup();
+void dx100_work5_loop();
 
