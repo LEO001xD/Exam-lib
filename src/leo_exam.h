70fcbbb932111aed62e70a4892189b4749c5d266
BooksHorse <august.ja11@gmail.com>
2024-02-20 14:54:16 +0700

    Update leo_exam.h

--- src/leo_exam.h
+++ src/leo_exam.h
@@ -1,12 +1,12 @@
-void 001xd_work2_setup();
-void 001xd_work2_loop();
+void xd_work2_setup();
+void xd_work2_loop();
 //-----------------------
-void 001xd_work3_setup();
-void 001xd_work3_loop();
+void xd_work3_setup();
+void xd_work3_loop();
 //-----------------------
-void 001xd_work4_setup();
-void 001xd_work4_loop();
+void xd_work4_setup();
+void xd_work4_loop();
 //-----------------------
-void 001xd_work5_setup();
-void 001xd_work5_loop();
+void xd_work5_setup();
+void xd_work5_loop();
 
