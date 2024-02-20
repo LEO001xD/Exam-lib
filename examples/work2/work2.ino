e84828437c62ad25b14789102d96ad335ddecfff
LEO001xD <141621669+LEO001xD@users.noreply.github.com>
2024-02-20 19:53:03 +0700

    Update work2.ino

--- examples/work2/work2.ino
+++ examples/work2/work2.ino
@@ -1,1 +1,11 @@
-
+#include <leo_exam.h>
+const uint8_t ldr = 2;
+const uint8_t leds[] = {2, 0, 4};
+void setup()
+{
+    work2::dx100_setup(ldr, leds, 3);
+}
+void loop()
+{
+    work2::dx100_loop(ldr, leds, 3);
+}
