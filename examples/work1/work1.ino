2a99065e36b0ffdc7a8d9aed7a3d41b2eb5a378f
LEO001xD <141621669+LEO001xD@users.noreply.github.com>
2024-02-20 20:18:12 +0700

    Update work1.ino

--- examples/work1/work1.ino
+++ examples/work1/work1.ino
@@ -2,9 +2,9 @@
 const uint8_t leds[] = {21, 19, 18, 5, 17};
 void setup()
 {
-    work2::dx100_setup(leds, 5);
+    work1::dx100_setup(leds, 5);
 }
 void loop()
 {
-    work2::dx100_loop(leds, 5);
+    work1::dx100_loop(leds, 5);
 }
