442b1abe5986246db5ca299e0455f555e3405b25
LEO001xD <141621669+LEO001xD@users.noreply.github.com>
2024-02-20 19:39:21 +0700

    Update leo_exam.cpp

--- src/leo_exam.cpp
+++ src/leo_exam.cpp
@@ -3,25 +3,23 @@
 #include <ESP32Servo.h>//work4
 #include <LiquidCrystal.h>//work5
 
-HCSR04 hc(16, 17);
-
 Servo servo;//work4
-
-void dx100_work2_setup(ldr_work2) {
+namespace work2{
+void dx100_setup(const int ldr_work2, const int led_work2[], const int arrayLength) {
   pinMode(ldr_work2,INPUT);
-  for (int i = 0; i < 3; i++) {
+  for (int i = 0; i < arrayLength; i++) {
     pinMode(led_work2[i], OUTPUT);
   }
   Serial.begin(115200);
 }
 
-void dx100_work2_loop() {
+void dx100_loop(const int ldr_work2, const int led_work2[], const int arrayLength) {
   int sun = analogRead(ldr_work2);
   Serial.print("sun >>");
   Serial.println(sun);
   delay(100);
   if (sun <= 1000) {
-    for (int i = 0; i < 3; i++) {
+    for (int i = 0; i < arrayLength; i++) {
     digitalWrite(led_work2[i], 1);
     }
   }
@@ -31,13 +29,16 @@ void dx100_work2_loop() {
     }
   }
 }
-void dx100_work3_setup() {
+}//namespace work2
+namespace work3{
+HCSR04 hc(16, 17);
+void dx100_setup(const int led1_work3, const int led2_work3, const int led3_work3) {
   pinMode(led1_work3, OUTPUT);
   pinMode(led2_work3, OUTPUT);
   pinMode(led3_work3, OUTPUT);
   Serial.begin(115200);
 }
-void dx100_work3_loop() {
+void dx100_loop(const int led1_work3, const int led2_work3, const int led3_work3) {
   int distance = hc.dist();
   Serial.println(distance); //return current distance (cm) in serial
   delay(100);
@@ -55,11 +56,15 @@ void dx100_work3_loop() {
     digitalWrite(led2_work3, LOW);
   }
 }
-void dx100_work4_setup() { 
+}//namespace work3
+namespace work4{
+HCSR04 hc(16, 17);
+Servo servo; 
+void dx100_setup(const int servo_work4) { 
   servo.attach(servo_work4, 500, 2400);
   Serial.begin(115200);
 }
-void dx100_work4_loop() {
+void dx100_loop() {
   int distance = hc.dist();
   Serial.println(distance); 
   delay(100);
@@ -74,13 +79,17 @@ void dx100_work4_loop() {
     delay(100);
   }
 }
+}//namespace work4
+namespace work5{
 int potenVal;
+HCSR04 hc(16, 17);
 
-void dx100_work5_setup() {
+void dx100_setup(const int potentio_work5, LiquidCrystal lcd) {
+  pinMode(potentio_work5, INPUT);
   lcd.begin(16, 2);
   Serial.begin(115200);
 }
-void dx100_work5_loop() {
+void dx100_loop(const int potentio_work5, LiquidCrystal lcd) {
   int distance = hc.dist();
   lcd.setCursor(0, 0);
   lcd.print("distance : "); 
@@ -98,3 +107,4 @@ void dx100_work5_loop() {
   Serial.print("potentio:");
   Serial.println(potenVal);
 }
+}//namespace work5
