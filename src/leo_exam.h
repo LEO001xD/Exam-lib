845860a5cc857681ec865cbff7cba799209557b4
LEO001xD <141621669+LEO001xD@users.noreply.github.com>
2024-02-20 11:26:39 +0700

    Update leo_exam.h

--- src/leo_exam.h
+++ src/leo_exam.h
@@ -3,3 +3,10 @@ void 001xd_work2_loop();
 //-----------------------
 void 001xd_work3_setup();
 void 001xd_work3_loop();
+//-----------------------
+void 001xd_work4_setup();
+void 001xd_work4_loop();
+//-----------------------
+void 001xd_work5_setup();
+void 001xd_work5_loop();
+
