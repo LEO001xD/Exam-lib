c0654a984b7cd9508884fb34f1f9a7bf4663519f
LEO001xD <141621669+LEO001xD@users.noreply.github.com>
2024-02-20 09:34:51 +0700

    Update leo_exam.cpp

--- src/leo_exam.cpp
+++ src/leo_exam.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include <HCSR04.h>
 //#define ldr_work2 34
 //int led_work2[3] = {19, 18, 17};
 void 001xd_work2_setup() {
@@ -9,7 +10,7 @@ void 001xd_work2_setup() {
   Serial.begin(115200);
 }
 
-void 001xd_work3_loop() {
+void 001xd_work2_loop() {
   int sun = analogRead(ldr_work2);
   Serial.print("sun >>");
   Serial.println(sun);
@@ -25,3 +26,29 @@ void 001xd_work3_loop() {
     }
   }
 }
+void 001xd_work3_setup() {
+  HCSR04 hc(16, 17); //initialisation class HCSR04 (trig pin , echo pin)void setup() {
+  pinMode(led1_work3, OUTPUT);
+  pinMode(led2_work3, OUTPUT);
+  pinMode(led3_work3, OUTPUT);
+  Serial.begin(115200);
+}
+void 001xd_work3_loop() {
+  int distance = hc.dist();
+  Serial.println(distance); //return current distance (cm) in serial
+  delay(60);
+  if (distance >= 0 && distance <= 5) {
+    digitalWrite(led1_work3, HIGH);
+    digitalWrite(led2_work3, LOW);
+    digitalWrite(led3_work3, LOW);
+  } else if (distance > 5 && distance < 11) {
+    digitalWrite(led2_work3, HIGH);
+    digitalWrite(led1_work3, LOW);
+    digitalWrite(led3_work3, LOW);
+  } else if (distance >= 11) {
+    digitalWrite(led3_work3, HIGH);
+    digitalWrite(led1_work3, LOW);
+    digitalWrite(led2_work3, LOW);
+  }
+}
+
