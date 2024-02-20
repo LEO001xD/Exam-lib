56303386c3db804d7e55fe5688af958778aaeeca
LEO001xD <141621669+LEO001xD@users.noreply.github.com>
2024-02-20 19:57:41 +0700

    Update work3.ino

--- examples/work3/work3.ino
+++ examples/work3/work3.ino
@@ -1,1 +1,12 @@
-
+#include <leo_exam.h>
+const uint8_t led1 = 2;
+const uint8_t led2 = 3;
+const uint8_t led3 = 4;
+void setup()
+{
+    work3::dx100_setup(led1, led2, led3);
+}
+void loop()
+{
+    work3::dx100_loop(led1, led2, led3);
+}
