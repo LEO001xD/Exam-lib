993946184cc3feaf8a5a3fdeaaadd2d381d7e20b
LEO001xD <141621669+LEO001xD@users.noreply.github.com>
2024-02-22 08:33:12 +0700

    Update work5.ino

--- examples/work5/work5.ino
+++ examples/work5/work5.ino
@@ -1,6 +1,7 @@
 #include <leo_exam.h>
 const uint8_t potentio_work5 = 2;
-LiquidCrystal lcd(12, 13, 16, 17, 18, 19);
+
+LiquidCrystal lcd(12, 13, 16, 17, 18, 19);//Vss=gnd VDD=3.3v 12=RS 13=E D4=16 D5=17 D6=18 D7=19
 void setup()
 {
     work5::dx100_setup(potentio_work5, lcd);
